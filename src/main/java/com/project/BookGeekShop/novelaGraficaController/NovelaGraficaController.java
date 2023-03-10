1146561e7fb87c35ce1eda985240ff85de02880f
adrix-10 <adrix1410@gmail.com>
2023-03-10 17:06:25 -0600

    Creación de Clases e Intefaces Vacias para realizar el proceso del CRUD
    Se crearon los archivos vacios necesarios para implementar el CRUD en el
    proyecto en las tablas: empleado, cliente, libro y novelaGrafica.

---
+++ src/main/java/com/project/BookGeekShop/novelaGraficaController/NovelaGraficaController.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.project.BookGeekShop.novelaGraficaController;
+
+/**
+ *
+ * @author adrix
+ */
+public class NovelaGraficaController {
+    
+}
