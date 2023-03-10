30c35ee89078337f19ebd5019ee588ffa05b1daa
Gono27 <59101439+Gono27@users.noreply.github.com>
2023-03-10 13:23:28 -0600

    Creación del proyecto y conexión con la base de datos

---
+++ src/test/java/com/project/BookGeekShop/BookGeekShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.BookGeekShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookGeekShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
